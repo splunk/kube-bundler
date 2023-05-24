bfd1bcb31a767ace1bbd486e47f6631af41b364d
Adam Lamar <alamar@splunk.com>
2023-05-24 15:05:51 -0600

    Update copyright headers

--- managers/registry.go
+++ managers/registry.go
@@ -1,3 +1,19 @@
+/*
+   Copyright 2023 Splunk Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package managers
 
 import (
@@ -16,10 +32,10 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/splunk/kube-bundler/api/v1alpha1"
 	"github.com/avast/retry-go"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
+	"github.com/splunk/kube-bundler/api/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
--- managers/status_node.go
+++ managers/status_node.go
@@ -1,3 +1,19 @@
+/*
+   Copyright 2023 Splunk Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package managers
 
 import (
@@ -46,7 +62,7 @@ var k0sNode = sync.Map{}
 var clusterNodesResults = sync.Map{}
 var clusterListTracker = sync.Map{}
 
-//the default value of the ssh daemonset port (changes to port are updated by application status)
+// the default value of the ssh daemonset port (changes to port are updated by application status)
 var sshPort = "2222"
 
 var clusterChecks = map[string]string{
